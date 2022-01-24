4e5ad23ca5ae74e6fce9562b78ff8c84d1a62481
cdiot <christopher.diot5@gmail.com>
2022-01-24 20:09:13 +0100

    Add pagination

--- controller.js
+++ controller.js
@@ -1,8 +1,17 @@
 class Controller {
     async index() {
-        let dataset = await Model.getList('https://reqres.in/api/users')
+        let dataset
+        let searchParams = new URLSearchParams(window.location.search);
+        let page = searchParams.get("page");
+        let id = searchParams.get("id");
+        if (page) {
+            dataset = await Model.get(`https://reqres.in/api/users?page=${page}`)
+        } else {
+            dataset = await Model.get('https://reqres.in/api/users')
+        }
+        console.log(dataset);
         let view = new View()
-        view.render(dataset.data)
+        view.render(dataset.data, dataset.total_pages)
     }
 }
 
--- view.js
+++ view.js
@@ -1,10 +1,14 @@
 class View {
-    render(users) {
-        const html = users
+    render(users, total_pages) {
+        let html = users
             .map(user => {
                 return `<p>Nom : ${user.first_name} ${user.last_name}</p>`;
             })
-            .join("");
+            .join("")
+        for (let i = 1; i <= total_pages; i++) {
+            console.log(i)
+            html += `<a href="index.html?page=${i}">page ${i}</a>`
+        }
         document
             .querySelector('#app')
             .insertAdjacentHTML('afterbegin', html);
